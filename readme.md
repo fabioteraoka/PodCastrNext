2c2868213e41303d5af969547b5a9f52cfecea3a
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-04-22 18:01:09 -0300

    Aula 4

---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+
+npm add rc-slider
+
+npm run dev
+npm run server
+npm run builder
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,39 @@
-
 import "../styles/global.scss";
 import { Header } from "../componets/Header";
-import styles from '../styles/app.module.scss';
+import styles from "../styles/app.module.scss";
 import { Player } from "../componets/Player";
+import { PlayerContext } from "../contexts/PlayerContext";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying,setIsPlaying] = useState(true);
+
+  function play(episode){
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state:boolean){
+    setIsPlaying(state);
+  }
+
   return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setIsPlaying}}>
     <div className={styles.wrapper}>
       <main>
-      <Header />
-      <Component {...pageProps} />
-      
+        <Header />
+        <Component {...pageProps} />
       </main>
       <Player />
-      
     </div>
-  )
+    </PlayerContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
